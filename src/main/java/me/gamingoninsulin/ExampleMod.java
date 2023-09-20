acbc4555cc87778645b075cae2d033d4eeb97ca8
GamingOnInsulin <gamingoninsulin@gmail.com>
2023-09-20 15:02:42 +0200

    Initial commit

---
+++ src/main/java/me/gamingoninsulin/ExampleMod.java
@@ -0,0 +1,18 @@
+package me.gamingoninsulin;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ExampleMod implements ModInitializer {
+    public static final Logger LOGGER = LoggerFactory.getLogger(Reference.MODID);
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("Starting " + Reference.MOD_NAME);
+		LOGGER.info("Mod Author is " + Reference.MOD_AUTHOR);
+		LOGGER.info("Datapack Owner is " + Reference.DATAPACK_OWNER);
+	}
+}
---
+++ src/main/java/me/gamingoninsulin/Reference.java
@@ -0,0 +1,10 @@
+package me.gamingoninsulin;
+
+public class Reference {
+
+    public static String MODID = "goi_airbender";
+    public static String MOD_NAME = "Elemental Benders";
+    public static String MOD_AUTHOR = "Gaming On Insulin";
+    public static String DATAPACK_OWNER = "Kodazu";
+}
+
