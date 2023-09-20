475897483af32f289270e5e34851e2c704fdb120
GamingOnInsulin <gamingoninsulin@gmail.com>
2023-09-20 15:15:06 +0200

    changed stuff
    - class name
    - mod_id

--- src/main/java/me/gamingoninsulin/Reference.java
+++ src/main/java/me/gamingoninsulin/Reference.java
@@ -2,7 +2,7 @@ package me.gamingoninsulin;
 
 public class Reference {
 
-    public static String MODID = "goi_airbender";
+    public static String MODID = "goi_elemetal_bender";
     public static String MOD_NAME = "Elemental Benders";
     public static String MOD_AUTHOR = "Gaming On Insulin";
     public static String DATAPACK_OWNER = "Kodazu";
